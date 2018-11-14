a6d5494c334df4c3fb4b0358f13025e869028aa5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-14 14:53:10 -0800

    added Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+import { CityLogo } from './ui/icons'; 
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-  <header className="header">
-    <div className="content-container">
-      <div className="header__content">
-        <Link className="header__title" to="/dashboard">
-          <h1>Boilerplate</h1>
-        </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
+  <AppBar
+    position='fixed'
+    style={{
+      background: '#98c5e9',
+      boxShadow: 'none',
+      padding: '10px 0',
+      borderBottom: '2px solid #00285e'
+    }}
+  >
+    <Toolbar style={{
+      display:'flex'
+    }}>
+      <div style={{ flexGrow: 1 }}>
+        <div className='header_logo'>
+          <CityLogo link={true} linkTo='/' width='70px' height='70px' />
+        </div>
       </div>
-    </div>
-  </header>
+
+      <Link to='/the_team'>
+        <Button color='inherit'>The Team</Button>
+      </Link>
+
+      <Link to='/the_matches'>
+      <Button color='inherit'>The Matches</Button>
+      </Link>
+    </Toolbar>
+  </AppBar>
 );
 
 const mapDispatchToProps = (dispatch) => ({
