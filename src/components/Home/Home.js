26ffffebada5059ef9a8d140d1376925e26e7f0e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-14 16:52:16 -0800

    coding featured component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Featured from './featured/Featured';
+
+const Home = () => {
+    return (
+        <div className='bck_blue'>
+            Home
+            <Featured />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Home/featured/Featured.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Stripes from './Stripes';
+
+
+const Featured = () => {
+    return (
+        <div className='featured_wrapper'>
+            Featured
+            <Stripes />
+        </div>
+    );
+};
+
+export default Featured;
