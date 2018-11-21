6bc194ea01d05fa9d5b4c1d9e1c736c918daf3b7
a-soliman <ahmed.soliman@programmer.net>
2018-11-21 11:01:25 -0800

    coded the blocks inner component

--- src/components/Home/matches/Blocks.js
+++ src/components/Home/matches/Blocks.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
 import { firebaseMatches } from '../../../firebase/firebase';
 import { firebaseLooper } from '../../ui/misc';
+import MatchesBlock from '../../ui/matches_block';
 
 class Blocks extends Component {
-    _isMounted = false;
     state = {
         matches: []
     }
 
-    getData = () => {
-        return firebaseMatches.limitToLast(6).once('value')
-            .then((snapshot) => {
-                this.setState({
-                    matches: firebaseLooper(snapshot).reverse()
-                })
-            });
+    getMatches = () => {
+        return new Promise((resolve, reject) => {
+            firebaseMatches.limitToLast(6).once('value')
+                .then((snapshot) => {
+                    resolve(firebaseLooper(snapshot));
+                });
+        })
     };
 
-    componentDidMount = () => {
-        console.log('mounting')
-        this.getData()
+    showMatches = (matches) => (
+        matches ? 
+            matches.map((match) => (
+                <Slide key={match.id} bottom>
+                    <div className="item">
+                        <div className="wrapper">
+                            <MatchesBlock match={match} />
+                        </div>
+                    </div>
+                </Slide>
+                
+            ))
+        : null
+    );
+
+    componentWillMount = () => {
+        this.getMatches().then(data => {
+            this.setState({
+                matches: data
+            })
+        })
     }
 
-    componentWillUnmount() {
-        console.log('unmounting');
-    }
 
     render() {
         return (
             <div className="home_matches">
-            something
+                {this.showMatches(this.state.matches)}
             </div>
         );
     }
