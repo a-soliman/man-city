c90009474d45f03316f2b7f0ff6b3c527ae1fb59
a-soliman <ahmed.soliman@programmer.net>
2018-11-21 16:29:43 -0800

    coded the text for meetplayers section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './featured/Featured';
 import MatchesHome from './matches/MatchesHome';
+import MeetPlayers from './meetPlayers/MeetPlayers';
 
 const Home = () => {
     return (
         <div className='bck_blue'>
             <Featured />
             <MatchesHome />
+            <MeetPlayers />
         </div>
     );
 };
