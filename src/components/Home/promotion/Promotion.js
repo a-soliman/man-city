91198c01124cbb30c845d80f8f39952c4d843eb8
a-soliman <ahmed.soliman@programmer.net>
2018-11-22 12:33:34 -0800

    coded the promotion section

---
+++ src/components/Home/promotion/Promotion.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Animation from './Animation';
+
+const Promotion = () => {
+    return (
+        <div className="promotion_wrapper" style={{ background: '#ffffff'}}>
+            <div className="container">
+                <Animation />
+            </div>
+        </div>
+    );
+};
+
+export default Promotion;
