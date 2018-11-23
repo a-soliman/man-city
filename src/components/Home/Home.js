3860e82c434f79701702f363caf6d97799d78068
a-soliman <ahmed.soliman@programmer.net>
2018-11-23 07:26:28 -0800

    completed Home route

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from './featured/Featured';
 import MatchesHome from './matches/MatchesHome';
 import MeetPlayers from './meetPlayers/MeetPlayers';
+import Promotion from './promotion/Promotion';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Featured />
             <MatchesHome />
             <MeetPlayers />
+            <Promotion />
         </div>
     );
 };
