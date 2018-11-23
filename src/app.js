60512456b41c3ebda5dac6e2f4d4c74f5963030d
a-soliman <ahmed.soliman@programmer.net>
2018-11-23 11:46:39 -0800

    edited the intial randering of the app

--- src/app.js
+++ src/app.js
@@ -26,17 +26,19 @@ const renderApp = () => {
 
 // ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
-renderApp()
-// firebase.auth().onAuthStateChanged((user) => {
-//   if (user) {
-//     store.dispatch(login(user.uid));
-//     renderApp();
-//     if (history.location.pathname === '/') {
-//       history.push('/dashboard');
-//     }
-//   } else {
-//     store.dispatch(logout());
-//     renderApp();
-//     history.push('/');
-//   }
-// });
+// renderApp()
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user)
+    store.dispatch(login(user.uid));
+    renderApp();
+    if (history.location.pathname === '/login') {
+      history.push('/dashboard');
+    }
+  } else {
+    console.log('no user');
+    store.dispatch(logout());
+    renderApp();
+    // history.push('/');
+  }
+});
