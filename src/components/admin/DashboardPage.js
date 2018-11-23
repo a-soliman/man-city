d5024ef2473796e6dbf4342cb1007f3714d48c1c
a-soliman <ahmed.soliman@programmer.net>
2018-11-23 11:48:01 -0800

    moved the dashboard page to admin

---
+++ src/components/admin/DashboardPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AdminLayout from '../Hoc/adminlayout';
+
+const DashboardPage = () => (
+  <AdminLayout>
+    <div className="user_dashboard">
+      <div>DashBoard</div>
+      Dashboard page content
+    </div>
+  </AdminLayout>
+  
+);
+
+export default DashboardPage;
