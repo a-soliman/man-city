b71f24e65a7e2304e82f9a4f5094829f6ae67cb7
a-soliman <ahmed.soliman@programmer.net>
2018-11-23 11:47:09 -0800

    added login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div className="box-layout">
-    <div className="box-layout__box">
-      <h1 className="box-layout__title">Boilerplate</h1>
-      <p>Tag line for app.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+  <div className="container">
+    <div className="signin_wrapper" style={{ margin: '100px' }}>
+
+          <h2>Please Login</h2>
+          <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
+  
 );
 
 const mapDispatchToProps = (dispatch) => ({
