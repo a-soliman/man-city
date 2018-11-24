4472598efa392370201df2faa351556f5147cd19
a-soliman <ahmed.soliman@programmer.net>
2018-11-23 18:30:15 -0800

    added a new route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import DashboardPage from '../components/admin/DashboardPage';
 import Matches from '../components/admin/Matches';
+import AddEditMatch from '../components/admin/AddEditMatch';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -21,6 +22,7 @@ const AppRouter = () => (
         <Route path="/" component={Home} exact={true} />
         <PublicRoute path="/login" component={LoginPage} exact={true} />
         <PrivateRoute path="/admin" component={DashboardPage} />
+        <PrivateRoute path="/admin_matches/edit_match/:id" component={AddEditMatch} />
         <PrivateRoute path="/admin_matches" component={Matches} />
         <Route component={NotFoundPage} />
       </Switch>
