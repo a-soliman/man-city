b5e6b73705945305508b510f0150b15267ad735c
a-soliman <ahmed.soliman@programmer.net>
2018-11-25 07:16:52 -0800

    fixed the overPrivate route issue

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -8,11 +8,7 @@ export const PublicRoute = ({
   ...rest
 }) => (
     <Route {...rest} component={(props) => (
-      isAuthenticated ? (
-        <Redirect to="/admin" />
-      ) : (
           <Component {...props} />
-        )
     )} />
   );
 
