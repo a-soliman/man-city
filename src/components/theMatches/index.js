d0ecea5849d8edcea79b26ede88ea2a4b45c9817
a-soliman <ahmed.soliman@programmer.net>
2018-11-25 09:09:50 -0800

    added matches filters

--- src/components/theMatches/index.js
+++ src/components/theMatches/index.js
@@ -12,7 +12,7 @@ class theMatches extends Component {
         loading: true,
         matches: [],
         filterdMatches: [],
-        playerFilter: 'All',
+        playedFilter: 'All',
         resultFilter: 'All'
     }
 
@@ -25,7 +25,22 @@ class theMatches extends Component {
                 filterdMatches: matches
             });
         });
-    }
+    };
+
+    showPlayed = (filter) => {
+        let filterdMatches;
+        if ( filter === 'All') filterdMatches = this.state.matches;
+        else filterdMatches = this.state.matches.filter(match => {
+            return match.final === filter
+        });
+
+        this.setState({
+            filterdMatches,
+            playedFilter: filter,
+            resultFilter: 'All'
+        });
+    };
+
     render() {
         const state = this.state;
         return (
@@ -33,7 +48,28 @@ class theMatches extends Component {
                 <div className="the_matches_wrapper">
                     <div className="left">
                         <div className="match_filters">
-                            <div className=""></div>
+                            <div className="match_filters_box">
+                                <div className="tag">
+                                    Show Match
+                                </div>
+                                <div className="cont">
+                                    <div 
+                                        className={`option ${state.playedFilter === 'All' ? 'active': ''}`} 
+                                        onClick={() => this.showPlayed('All')}>
+                                        All
+                                    </div>
+                                    <div 
+                                        className={`option ${state.playedFilter === 'Yes' ? 'active': ''}`}
+                                        onClick={() => this.showPlayed('Yes')}>
+                                        Played
+                                    </div>
+                                    <div 
+                                        className={`option ${state.playedFilter === 'No' ? 'active': ''}`} 
+                                        onClick={() => this.showPlayed('No')}>
+                                        Not Played
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <MatchesList matches={state.filterdMatches} />
                     </div>
